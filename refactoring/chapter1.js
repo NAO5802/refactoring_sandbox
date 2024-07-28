089bd2fc5f4976a9e4ef429a67da71b0f784c1ee
NAO5802 <i100185b@gmail.com>
2024-07-28 21:06:13 +0900

    Extract specify the play logic

--- refactoring/chapter1.js
+++ refactoring/chapter1.js
@@ -31,8 +31,12 @@ export function statement(invoice, plays) {
         return result;
     }
 
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         const thisAmount = amountFor(perf, play);
         // ボリューム特典のポイントを加算
         volumeCredits += Math.max(perf.audience - 30, 0);
