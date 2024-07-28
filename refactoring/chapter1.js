a1bf37667071190afee91c083d166e03120f45ca
NAO5802 <i100185b@gmail.com>
2024-07-28 21:39:31 +0900

    Slide statement

--- refactoring/chapter1.js
+++ refactoring/chapter1.js
@@ -1,6 +1,5 @@
 export function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
     function usd(aNumber) {
@@ -51,6 +50,7 @@ export function statement(invoice, plays) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
+    let volumeCredits = 0;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
     }
