b0b9fb6bc204f86c5085ca7508a253cf4886ef52
NAO5802 <i100185b@gmail.com>
2024-07-28 21:11:37 +0900

    Remove unused argument

--- refactoring/chapter1.js
+++ refactoring/chapter1.js
@@ -9,7 +9,7 @@ export function statement(invoice, plays) {
         minimumFractionDigits: 2
     }).format;
 
-    function amountFor(aPerformance, play) {
+    function amountFor(aPerformance) {
         let result = 0;
         switch (playFor(aPerformance).type) {
             case "tragedy":
