a4f85dc2dfa948d5cf6e294d1c862758cf05a163
NAO5802 <i100185b@gmail.com>
2024-07-30 09:26:49 +0900

    Remove unused argument

--- refactoring/chapter1.js
+++ refactoring/chapter1.js
@@ -1,4 +1,4 @@
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data.performances) {
         // 注文の内訳を出力
@@ -47,7 +47,7 @@ export function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
-    return renderPlainText(statementData, plays);
+    return renderPlainText(statementData);
 
     function enrichPerformance(aPerformance){
         const result = Object.assign({}, aPerformance);
