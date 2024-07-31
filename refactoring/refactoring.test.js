c44f2a2d67b6f60154d7d9a98441b0680d4bc5d1
NAO5802 <i100185b@gmail.com>
2024-07-31 09:08:23 +0900

    Add test for createStatementData

--- refactoring/refactoring.test.js
+++ refactoring/refactoring.test.js
@@ -1,7 +1,8 @@
-import { expect, test } from 'vitest'
+import {expect, test, describe} from 'vitest'
 import {statement} from "./chapter1"
+import createStatementData from "./createStatementData";
 
-test('statement', () => {
+describe('chapter1', () => {
     const invoice = [
         {
             "customer": "BigCo",
@@ -35,7 +36,51 @@ test('statement', () => {
             "type": "tragedy"
         }
     }
-    expect(statement(invoice[0], plays)).toBe("Statement for BigCo\n Hamlet: $650.00 (55 seats)\n As You Like It: $580.00 (35 seats)\n Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n")
+
+    test('statement', () => {
+        expect(statement(invoice[0], plays)).toBe("Statement for BigCo\n Hamlet: $650.00 (55 seats)\n As You Like It: $580.00 (35 seats)\n Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n")
+    })
+
+    test('createStatementData', () => {
+        const expected = {
+            customer: 'BigCo',
+            performances: [
+                {
+                    playID: 'hamlet',
+                    audience: 55,
+                    play: {
+                        "name": "Hamlet",
+                        "type": "tragedy"
+                    },
+                    amount: 65000,
+                    volumeCredits: 25
+                },
+                {
+                    playID: 'as−like',
+                    audience: 35,
+                    play: {
+                        "name": "As You Like It",
+                        "type": "comedy"
+                    },
+                    amount: 58000,
+                    volumeCredits: 12
+                },
+                {
+                    playID: 'othello',
+                    audience: 40,
+                    play: {
+                        "name": "Othello",
+                        "type": "tragedy"
+                    },
+                    amount: 50000,
+                    volumeCredits: 10
+                }
+            ],
+            totalAmount: 173000,
+            totalVolumeCredits: 47
+        }
+        expect(createStatementData(invoice[0], plays)).toEqual(expected)
+    })
 })
 
 
