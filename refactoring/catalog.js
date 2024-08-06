f1d3fd9a1a1572e6900cfa9e7b32a60b0c3deebf
NAO5802 <i100185b@gmail.com>
2024-08-06 09:27:13 +0900

    Remove unnecessary function

--- refactoring/catalog.js
+++ refactoring/catalog.js
@@ -1,18 +1,8 @@
 export function printOwing(invoice) {
-    printBanner();
-
     const outstanding = calculateOutstanding(invoice);
     invoice.dueDate = recordDueDate();
 
-    printDetails(invoice, outstanding);
-
     return printResult(invoice, outstanding);
-
-    function printBanner() {
-        console.log("***********************");
-        console.log("**** Customer Owes ****");
-        console.log("***********************");
-    }
 
     function calculateOutstanding(invoice) {
         let outstanding = 0;
@@ -25,12 +15,6 @@ export function printOwing(invoice) {
     function recordDueDate() {
         const today = Clock.today;
         return new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-    }
-
-    function printDetails(invoice, outstanding) {
-        console.log(`name: ${invoice.customer}`);
-        console.log(`amount: ${outstanding}`);
-        console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
     }
 
     function printResult(invoice, outstanding) {
