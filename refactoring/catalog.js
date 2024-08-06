23852b5cf1685248cdf2d34cca45f3a372c7ba89
NAO5802 <i100185b@gmail.com>
2024-08-06 09:17:52 +0900

    Move functions

--- refactoring/catalog.js
+++ refactoring/catalog.js
@@ -1,11 +1,21 @@
 export function printOwing(invoice) {
+    printBanner();
+
+    const outstanding = calculateOutstanding(invoice);
+
+    // 締め日の記録（record due date）
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+
+    printDetails();
+
+    return `***********************\n**** Customer Owes ****\n***********************\nname: ${invoice.customer}\namount: ${outstanding}\ndue: ${invoice.dueDate.toLocaleDateString()}`;
+
     function printBanner() {
         console.log("***********************");
         console.log("**** Customer Owes ****");
         console.log("***********************");
     }
-
-    printBanner();
 
     function calculateOutstanding(invoice) {
         let outstanding = 0;
@@ -15,23 +25,11 @@ export function printOwing(invoice) {
         return outstanding;
     }
 
-    // 未払い金の計算 （calculate outstanding）
-    const outstanding = calculateOutstanding(invoice);
-
-    // 締め日の記録（record due date）
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
     function printDetails() {
         console.log(`name: ${invoice.customer}`);
         console.log(`amount: ${outstanding}`);
         console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
     }
-
-    // 明細の印字 （print details）
-    printDetails();
-
-    return `***********************\n**** Customer Owes ****\n***********************\nname: ${invoice.customer}\namount: ${outstanding}\ndue: ${invoice.dueDate.toLocaleDateString()}`;
 }
 
 class Clock {
