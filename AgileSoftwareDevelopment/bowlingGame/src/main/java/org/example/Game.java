9a55d67677f788f4bffafdbd4adeb1c35b4bee9e
NAO5802 <i100185b@gmail.com>
2025-03-10 08:47:00 +0900

    implement scoreForFrame

--- AgileSoftwareDevelopment/bowlingGame/src/main/java/org/example/Game.java
+++ AgileSoftwareDevelopment/bowlingGame/src/main/java/org/example/Game.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Game {
     private int itsScore = 0;
+    private int[] itsThrows = new int[21];
 
     int score() {
         return itsScore;
@@ -11,7 +12,14 @@ public class Game {
         itsScore += pins;
     }
 
-    int scoreForFrame(int frame) {
-        return 0;
+    int scoreForFrame(int theFrame) {
+        int ball = 0;
+        int score = 0;
+        for(int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
+            int firstThrow = itsThrows[ball++];
+            int secondThrow = itsThrows[ball++];
+            score += firstThrow + secondThrow;
+        }
+        return score;
     }
 }
