17878d0628d9c110b55a499a80474b84911a7de6
NAO5802 <i100185b@gmail.com>
2025-03-12 08:57:16 +0900

    extract method to Scorer

--- AgileSoftwareDevelopment/bowlingGame/src/main/java/org/example/Game.java
+++ AgileSoftwareDevelopment/bowlingGame/src/main/java/org/example/Game.java
@@ -2,18 +2,16 @@ package org.example;
 
 public class Game {
     private int itsScore = 0;
-    private int[] itsThrows = new int[21];
-    private int itsCurrentThrow = 0;
     private int itsCurrentFrame = 1;
     private boolean firstThrowInFrame = true;
-    private int ball;
+    private Scorer itsScorer = new Scorer();
 
     int score() {
         return scoreForFrame(getCurrentFrame() - 1);
     }
 
     void add(int pins) {
-        itsThrows[itsCurrentThrow++] = pins;
+        itsScorer.addThrow(pins);
         itsScore += pins;
         adjustCurrentFrame(pins);
     }
@@ -33,42 +31,7 @@ public class Game {
     }
 
     int scoreForFrame(int theFrame) {
-        ball = 0;
-        int score = 0;
-        for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
-            if (strike()) {
-                score += 10 + nextTwoBallsForStrike();
-                ball++;
-            } else if (spare()) {
-                score += 10 + nextBallForSpare();
-                ball += 2;
-            } else {
-                score += twoBallsInFrame();
-                ball += 2;
-            }
-
-        }
-        return score;
-    }
-
-    private int nextTwoBallsForStrike() {
-        return itsThrows[ball + 1] + itsThrows[ball + 2];
-    }
-
-    private boolean strike() {
-        return itsThrows[ball] == 10;
-    }
-
-    private int twoBallsInFrame() {
-        return itsThrows[ball] + itsThrows[ball + 1];
-    }
-
-    private int nextBallForSpare() {
-        return itsThrows[ball + 2];
-    }
-
-    private boolean spare() {
-        return (itsThrows[ball] + itsThrows[ball + 1]) == 10;
+        return itsScorer.scoreForFrame(theFrame);
     }
 
     int getCurrentFrame() {
