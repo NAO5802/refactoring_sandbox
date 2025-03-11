31ad9dee334ca144f37dd86b8aa470da2aa90398
NAO5802 <i100185b@gmail.com>
2025-03-11 09:18:27 +0900

    refactor strike

--- AgileSoftwareDevelopment/bowlingGame/src/main/java/org/example/Game.java
+++ AgileSoftwareDevelopment/bowlingGame/src/main/java/org/example/Game.java
@@ -38,9 +38,10 @@ public class Game {
         ball = 0;
         int score = 0;
         for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
-            firstThrow = itsThrows[ball++];
-            if (firstThrow == 10) { // ストライク
-                score += 10 + itsThrows[ball] + itsThrows[ball + 1];
+            firstThrow = itsThrows[ball];
+            if (strike()) { // ストライク
+                ball++;
+                score += 10 + nextTwoBalls();
             } else {
                 score += handleSecondThrow();
             }
@@ -48,14 +49,24 @@ public class Game {
         return score;
     }
 
+    private int nextTwoBalls() {
+        return itsThrows[ball] + itsThrows[ball + 1];
+    }
+
+    private boolean strike() {
+        return itsThrows[ball] == 10;
+    }
+
     private int handleSecondThrow() {
         int score = 0;
-        secondThrow = itsThrows[ball++];
+        secondThrow = itsThrows[ball + 1];
 
         int frameScore = firstThrow + secondThrow;
         if (frameScore == 10) {
+            ball += 2;
             score += frameScore + itsThrows[ball];
         } else {
+            ball += 2;
             score += frameScore;
         }
         return score;
